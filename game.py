a49d98dc9145b29ee31c8842a52b0d4db8aa3df5
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-09-12 22:58:02 -0400

    Initial commit. Wrote first and second locations. Added first location to main.

---
+++ game.py
@@ -0,0 +1,26 @@
+# Intro to Programming
+# Author: Anthony Griggs
+# Date: 9/12/17
+
+#iScore = 0
+gLocStart = "You awake to find a white empty space. You seem to be floating in an exetensial 'nothingness'. A chill runs down your spine: what exactly is this place, this unfamiliar void?"
+#def IncScore(i):
+#   i = i + 5
+#    return i
+def main():
+    #My, my
+    iScore = 0 #For some reason iScore doesn't work outside the function...
+    print(iScore)
+    print(gLocStart)
+    print()
+    print("Ah, some brain activity!")
+    print("Well, well, look who's awake! Hey! Can you here me?")
+    input("<Press enter to awake>")
+    #Well, well
+    #iScore = iScore + 5
+    iScore = 5
+
+
+main()
+    
+
